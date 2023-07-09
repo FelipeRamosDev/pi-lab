7ca0d051d6d4aa32ea4effa0df1d7a3e40071e03
FelipeRamos1202 <felipe.ramos@osf.digital>
2023-07-09 19:25:37 -0300

    [PI-5] Create a Socket connection

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+import { Server } from 'socket.io';
 
+const io = new Server({
+    cors: {
+        origin: ['http://localhost:3000']
+    }
+});
+
+io.on('connection', connected => {
+    console.log(connected.id);
+
+    connected.on('motor:power', (state) => {
+        console.log('Motor power:', state)
+    });
+
+    connected.on('test:connection', () => {
+        console.log('Connection state:', 'connected')
+        connected.emit('testFE:connection', { currentState: 'connected' });
+    });
+
+});
+
+
+io.listen(5555);
